1dcce1ca80332a85deef96854968517591098285
leenajayavarshiniP <leenajayavarshinip@gmail.com>
2026-07-31 11:56:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
 URL = "https://www.fresheroffcampus.com/"
-
 DATA_FILE = "data/jobs.json"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
